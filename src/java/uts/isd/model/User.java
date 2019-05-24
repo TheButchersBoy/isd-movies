d778b2e0e96d51e1c9c5bbd64dc0a57649b532a2
KyleZingerBurg <kyle_zeng@hotmail.com>
2019-05-25 03:04:58 +1000

    Update User.java
    updated user.java

--- src/java/uts/isd/model/User.java
+++ src/java/uts/isd/model/User.java
@@ -4,20 +4,72 @@ import java.io.Serializable;
 
 public class User implements Serializable{ // TODO: Do we need to implement Serializable for models?
     
-    private String name;
+    private String id;
     private String email;
     private String password;
+    private String firstName;
+    private String lastName;
     private String dob;
     
     public User() {
         super();
     }
     
-    public User(String name, String email, String password, String dob) {
+    public User(String id, String email, String password, String firstName, String lastName, String dob) {
         super();
-        this.name = name;
+        this.id = id;
         this.email = email;
         this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+    }
+    
+    public String getID() {
+        return id;
+    }
+    
+    public void setID(String id) {
+        this.id = id;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getDOB() {
+        return dob;
+    }
+    
+    public void setDOB(String dob) {
         this.dob = dob;
     }
     
